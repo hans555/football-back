757cda169b1820d8598281a370bb4c2f656716ba
hans555 <onghansheng@gmail.com>
2022-07-15 13:30:54 +0800

    add new package

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+require("dotenv").config();
+
+var port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.post("/url/post", handlePostUrl);
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});

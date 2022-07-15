4277b51a87f8df27757005f381250cfb59d5a156
hans555 <onghansheng@gmail.com>
2022-07-15 17:08:34 +0800

    update match score logic

--- footballHandler.js
+++ footballHandler.js
@@ -9,8 +9,34 @@ async function handlePostMatch(req, res) {
 
   const client = await pool.connect();
   try {
+    matches.forEach((match) => {
+      const {team1, team2, score1, score2 } = match;
+      teams.team1.total_match_goal += score1
+      teams.team2.total_match_goal += score2
+      if (score1 > score2) {
+        teams[team1].total_match_point += 3
+        teams[team1].alternate_total_match_point += 5
+        teams[team2].alternate_total_match_point += 1
+      } else if (match.score1 < match.score2) {
+        teams[team2].total_match_point += 3
+        teams[team2].alternate_total_match_point += 5
+        teams[team1].alternate_total_match_point += 1
+      } else {
+        teams[team1].total_match_point += 1
+        teams[team2].total_match_point += 1
+        teams[team1].alternate_total_match_point += 3
+        teams[team2].alternate_total_match_point += 3
+      }
+    });
+    
+    console.log(teams);
+    await client.query("BEGIN");
+    insertTeamQueryString = getPostTeamQuery(teams);
+
     res.status(200).json({});
+    await client.query("COMMIT");
   } catch (err) {
+    await client.query("ROLLBACK");
     console.log(err);
     res.status(400).end();
   } finally {
--- footballQueries.js
+++ footballQueries.js
@@ -1,14 +1,19 @@
 const format = require("pg-format");
 
 function getPostTeamQuery(teams) {
-	return format("INSERT INTO teams VALUES %L", teams)
+  return format("INSERT INTO teams VALUES %L", teams);
 }
 
 function getTeamRankingQuery() {
-	return "SELECT * FROM teams"
+  return "SELECT * FROM teams";
+}
+
+function getDeleteTeamQuery() {
+  return "TRUNCATE TABLE teams";
 }
 
 module.exports = {
-	getPostTeamQuery,
-  getTeamRankingQuery
-}
+  getPostTeamQuery,
+  getTeamRankingQuery,
+  getDeleteTeamQuery,
+};
